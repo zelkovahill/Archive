9b7555d65bc8626e6a08a53890967d5c93dfac1e
Chang WooMin <changwoomin426@gmail.com>
2025-01-10 17:40:11 +0900

    Create LinearQueue.h

---
+++ C++/Queue/LinearQueue.h
@@ -0,0 +1,33 @@
+#ifndef LINEARQUEUE_H
+#define LINEARQUEUE_H
+
+#include <iostream>
+
+struct Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int value) : data(value), next(nullptr) {}
+};
+
+class LinearQueue
+{
+private:
+    Node *front;
+    Node *rear;
+    int size;
+
+public:
+    LinearQueue();
+    ~LinearQueue();
+
+    bool isEmpty() const;
+    void enqueue(int value);
+    int dequeue();
+    int peek() const;
+    void printQueue() const;
+};
+
+#endif // LINEARQUEUE_H
